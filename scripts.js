c6d7dbe99b2ddf2d77a284654b5812a80d256781
brad coore <62188837+bradcoore@users.noreply.github.com>
2021-10-21 15:30:07 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+window.onload=function(){
+    const button= document.querySelector("button");
+
+    button.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let msg=document.getElementsByClassName("message")[0];
+    let email= document.getElementById('email').value;
+
+    if (email===''){
+        msg.innerHTML=('Please enter a valid email address');
+    }else{
+        msg.innerHTML= `Thank you! Your email address ${email} has been added to our mailing list.`;
+        console.log("Form has been submitted");
+    }    
+})
+
+}
